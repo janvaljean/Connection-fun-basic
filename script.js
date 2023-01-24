9eba5de99cf59096150baa773f454e6e44869378
janvaljean <kurtuldu@gmx.de>
2023-01-24 22:14:42 +0100

    low updated

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ const voice = document.querySelector('.voice');
 const voice2 = document.querySelector('.voice2');
 const voice3 = document.querySelector('.voice3');
 
+//!mouse ile resmin üzerine gelince
+picture.onmouseover = () => {
+  picture.src = "../img/aslan1.jpeg";
+};
+
+//!mouse ile resmin üzerinden gidince
+picture.onmouseout = () => {
+  picture.src = "../img/aslan2.jpeg";
+};
+
+
+
 //!rufen button
 document.querySelector('.rufen').onclick = () => {
     picture.src = "../img/img.gif";
